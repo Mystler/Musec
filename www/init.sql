d6e5bc19fbb9a4a0070a6dbb96d826171b393a3d
Florian Meißner <developer@mystler.eu>
2013-08-02 11:19:29 +0200

    Convert SQL scripts to UTF-8

--- www/init.sql
+++ www/init.sql
@@ -1,6 +1,6 @@
 /*==LICENSE==
 This file is part of Musec.
-Copyright (C) 2013 Florian Meiﬂner
+Copyright (C) 2013 Florian Mei√üner
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
