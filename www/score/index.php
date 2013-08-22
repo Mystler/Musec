8d3827787b8bb02d0a1bf872534d6b714b957e84
Florian Meißner <developer@mystler.eu>
2013-08-23 00:03:51 +0200

    Instruct HTML to use UTF-8 too

--- www/score/index.php
+++ www/score/index.php
@@ -18,7 +18,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ==LICENSE==*/
 ?>
 <html>
-<head><title>Musec Score</title>
+<head>
+<title>Musec Score</title>
+<meta charset="UTF-8">
 <style>
 body {
     text-align: center;
