9d6f8868325bed53463890f8063efcfe35f945a1
Florian Meißner <developer@mystler.eu>
2013-08-22 23:53:43 +0200

    Add License text to PHP files ... because my last name forces UTF-8

--- www/score/index.php
+++ www/score/index.php
@@ -1,3 +1,22 @@
+<?php
+/*==LICENSE==
+This file is part of Musec.
+Copyright (C) 2013 Florian Meißner
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+==LICENSE==*/
+?>
 <html>
 <head><title>Musec Score</title>
 <style>
@@ -35,40 +54,40 @@ in the client. Entries for existing users will be overwritten.<br><br>
         <th class="left">Date</th>
     </tr>
 <?php
-    require_once "config.ini.php";
+require_once "config.ini.php";
 
-    $dbconn = pg_connect("host={$dbhost} dbname={$dbname} user={$dbuser} password={$dbpass}");
-    if (!$dbconn) {
-        echo "Database connection error!";
-        exit;
-    }
+$dbconn = pg_connect("host={$dbhost} dbname={$dbname} user={$dbuser} password={$dbpass}");
+if (!$dbconn) {
+    echo "Database connection error!";
+    exit;
+}
 
-    $result = pg_query($dbconn, "SELECT username, score, average, played, bingo, streak,
-            difficulty, to_char(scoredate, 'DD Mon YYYY, HH12:MI AM') FROM scores
-            ORDER BY score DESC, average DESC, streak DESC, bingo DESC, played DESC");
-    if (!$result) {
-        echo "Error on fetching data!";
-        pg_close($dbconn);
-        exit;
-    }
+$result = pg_query($dbconn, "SELECT username, score, average, played, bingo, streak,
+        difficulty, to_char(scoredate, 'DD Mon YYYY, HH12:MI AM') FROM scores
+        ORDER BY score DESC, average DESC, streak DESC, bingo DESC, played DESC");
+if (!$result) {
+    echo "Error on fetching data!";
+    pg_close($dbconn);
+    exit;
+}
 
-    while ($row = pg_fetch_row($result)) {
+while ($row = pg_fetch_row($result)) {
 ?>
     <tr>
         <td><?php echo $row[0]; ?></td>
-        <td class="center"><?php echo $row[1]; ?></td>
-        <td class="center"><?php echo $row[2]; ?></td>
-        <td class="center"><?php echo $row[3]; ?></td>
-        <td class="center"><?php echo $row[4]; ?></td>
-        <td class="center"><?php echo $row[5]; ?></td>
-        <td class="center"><?php echo $row[6]; ?></td>
+        <td class="center"><?php echo $row[1];?></td>
+        <td class="center"><?php echo $row[2];?></td>
+        <td class="center"><?php echo $row[3];?></td>
+        <td class="center"><?php echo $row[4];?></td>
+        <td class="center"><?php echo $row[5];?></td>
+        <td class="center"><?php echo $row[6];?></td>
         <td><?php echo $row[7]; ?></td>
     </tr>
 <?php
-    }
-    pg_free_result($result);
+}
+pg_free_result($result);
 
-    pg_close($dbconn);
+pg_close($dbconn);
 ?>
 </table>
 </body>
--- www/score/submit.php
+++ www/score/submit.php
@@ -1,28 +1,46 @@
 <?php
-    $muser = $_POST["user"];
-    $mavg = $_POST["avg"];
-    $mscore = $_POST["score"];
-    $mplayed = $_POST["played"];
-    $mbingo = $_POST["bingo"];
-    $mstreak = $_POST["streak"];
-    $mdiff = $_POST["diff"];
+/*==LICENSE==
+This file is part of Musec.
+Copyright (C) 2013 Florian Meißner
 
-    if (!isset($muser) || !isset($mavg) || !isset($mscore) || !isset($mplayed) ||
-            !isset($mbingo) || !isset($mstreak) || !isset($mdiff))
-        die("Invalid request!");
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    require_once "config.ini.php";
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    $dbconn = pg_connect("host={$dbhost} dbname={$dbname} user={$dbuser} password={$dbpass}");
-    if (!$dbconn)
-        die("Database connection error!");
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+==LICENSE==*/
 
-    $result = pg_query($dbconn, "SELECT addscore('{$muser}', {$mavg}, {$mscore}, {$mplayed},
-                       {$mbingo}, {$mstreak}, '{$mdiff}')");
-    if (!$result) {
-        pg_close($dbconn);
-        die("Error on submitting data!");
-    }
+$muser = $_POST["user"];
+$mavg = $_POST["avg"];
+$mscore = $_POST["score"];
+$mplayed = $_POST["played"];
+$mbingo = $_POST["bingo"];
+$mstreak = $_POST["streak"];
+$mdiff = $_POST["diff"];
 
+if (!isset($muser) || !isset($mavg) || !isset($mscore) || !isset($mplayed) ||
+        !isset($mbingo) || !isset($mstreak) || !isset($mdiff))
+    die("Invalid request!");
+
+require_once "config.ini.php";
+
+$dbconn = pg_connect("host={$dbhost} dbname={$dbname} user={$dbuser} password={$dbpass}");
+if (!$dbconn)
+    die("Database connection error!");
+
+$result = pg_query($dbconn, "SELECT addscore('{$muser}', {$mavg}, {$mscore}, {$mplayed},
+                   {$mbingo}, {$mstreak}, '{$mdiff}')");
+if (!$result) {
     pg_close($dbconn);
+    die("Error on submitting data!");
+}
+
+pg_close($dbconn);
 ?>
